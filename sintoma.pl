c3f171742da9c8bd191b6dd865abffce1923d2bc
cbz1902 <carlitosbenitez0095@gmail.com>
2019-05-19 17:55:38 -0400

    Se actualizo el dataset y se agrego una captura de la regla encontrada

--- sintoma.pl
+++ sintoma.pl
@@ -1,9 +1,11 @@
 foil_predicates([sintoma/2, sintoma_dengue/1, produce/2, '='/2]).
-foil_cwa(false).		 % No usa la hipotesis del mundo cerrado
-foil_use_negations(true). 	 % No usa informacion negativa
-foil_det_lit_bound(0). 		 % No aï¿½ade literales determinados
 
-verbosity(2).                    % Devuelve la lista de ejemplos positivos y negativos cubiertos.
+
+foil_cwa(false).		 % No usa la hipotesis del mundo cerrado
+foil_use_negations(false). 	 % No usa informacion negativa
+foil_det_lit_bound(0). 		 % No añade literales determinados
+
+verbosity(2).          % Devuelve la lista de ejemplos positivos y negativos cubiertos
 
 
 % Ejemplos positivos
@@ -15,30 +17,37 @@ sintoma(nauseas, dengue_clasico).
 sintoma(vomitos, dengue_clasico).
 sintoma(erupciones, dengue_clasico).
 sintoma(sangrados, dengue_hemorragico).
-sintoma(acumulacion_liquidos_sangre, dengue_hemorragico).
+sintoma(acumulacion_liquidos_sange, dengue_hemorragico).
 sintoma(dificultad_respiratoria, dengue_hemorragico).
 sintoma(dolor_abdominal_intenso, dengue_hemorragico).
 sintoma(respiracion_acelerada, dengue_hemorragico).
 sintoma(vomitos_con_sangre, dengue_hemorragico).
 
 
-% Ejemplos negativos.
-sintoma(fiebre, resfriado) :- fail.
-sintoma(acumulacion_liquidos, hidropesia) :- fail.
-sintoma(dolor_cabeza,migranha):- fail.
+% Ejemplos negativos
+sintoma(fiebre, resfriado) :-fail.
+sintoma(acumulacion_liquidos, hidropesia) :-fail.
 
 
-% Conocimiento base.
+% Conocimiento base
 sintoma_dengue(fiebre_elevada).
-sintoma_dengue(dolor_cabeza).
-sintoma_dengue(vomitos).
-sintoma_dengue(dolor_muscular).
+sintoma_dengue(dolor_cabeza_intenso).
+sintoma_dengue(dolores_articulares).
+sintoma_dengue(dolores_musculares).
 sintoma_dengue(dolor_ocular).
-sintoma_dengue(dolor_articular).
+sintoma_dengue(nauseas).
 sintoma_dengue(erupciones).
+sintoma_dengue(sangrados).
+sintoma_dengue(acumulacion_liquidos_sange).
+sintoma_dengue(dificultad_respiratoria).
+sintoma_dengue(dolor_abdominal_intenso).
+sintoma_dengue(respiracion_acelerada).
+sintoma_dengue(vomitos_con_sangre).
 
 produce(dengue_hemorragico, sangrados).
 produce(dengue_clasico, dolor_cabeza_intenso).
+
+
 
 
 
